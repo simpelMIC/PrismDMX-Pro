53ee0c6d838cf4971dfe8c7ec64e95812f57dcc9
simpelMIC <mic-pro@online.de>
2024-05-31 16:06:04 +0200

    Initial Commit

---
+++ PrismDMXPro/PrismDMXProApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrismDMXProApp.swift
+//  PrismDMXPro
+//
+//  Created by Christian on 31.05.24.
+//
+
+import SwiftUI
+
+@main
+struct PrismDMXProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
